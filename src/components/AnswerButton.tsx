ea8bc75ba17bf03a0534811951a6b6ecb1ac3722
Irene2828 <iryna.herts@gmail.com>
2026-06-26 00:19:43 -0400

    Update answer buttons to have 20% opacity white glass background

--- src/components/AnswerButton.tsx
+++ src/components/AnswerButton.tsx
@@ -53,7 +53,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   defaultButton: {
-    backgroundColor: 'transparent',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
     borderColor: '#374151',
   },
   defaultText: {
