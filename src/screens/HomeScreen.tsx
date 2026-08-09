12a35d9bca4e3adf6c8750b1432c6487d2dbd16c
Irene2828 <iryna.herts@gmail.com>
2026-08-09 16:51:18 -0400

    fix: Restore nested Animated.View structure to prevent fatal white screen crash on iOS caused by mixing native and non-native Animated values in a single transform array

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -134,8 +134,6 @@ const AstronautHero = ({ reduceMotion, touchDisplacement }: { reduceMotion: bool
           { translateY: floatY },
           { translateX: touchDisplacement.x },
           { translateY: touchDisplacement.y },
-          { translateX: dragTranslateX },
-          { translateY: dragTranslateY },
           { rotate: rotateDeg }
         ],
         alignItems: 'center',
@@ -144,12 +142,21 @@ const AstronautHero = ({ reduceMotion, touchDisplacement }: { reduceMotion: bool
         touchAction: 'none'
       }}
     >
-      <Image 
-        source={require('../../assets/mascot_v2_transparent.png')} 
-        style={{ width: 110, height: 110, resizeMode: 'contain', zIndex: 10 }} 
-        pointerEvents="none"
-        draggable={false}
-      />
+      <Animated.View style={{
+        transform: [
+          { translateX: dragTranslateX },
+          { translateY: dragTranslateY }
+        ],
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Image 
+          source={require('../../assets/mascot_v2_transparent.png')} 
+          style={{ width: 110, height: 110, resizeMode: 'contain', zIndex: 10 }} 
+          pointerEvents="none"
+          draggable={false}
+        />
+      </Animated.View>
     </Animated.View>
   );
 };
